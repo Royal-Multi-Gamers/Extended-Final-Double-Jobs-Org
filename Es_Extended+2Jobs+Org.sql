a7f81b1aa106e2c058bd8471841146393b01e785
Destructor <thibaut.lambart@live.fr>
2020-12-03 17:29:20 +0100

    Upload ALL

---
+++ Es_Extended+2Jobs+Org.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : jeu. 03 déc. 2020 à 17:19
+-- Version du serveur :  10.3.25-MariaDB-0+deb10u1
+-- Version de PHP : 7.3.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `org`
+--
+
+CREATE TABLE `org` (
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `organisation_moneywash`
+--
+
+CREATE TABLE `organisation_moneywash` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(60) COLLATE utf8mb4_bin NOT NULL,
+  `society` varchar(60) COLLATE utf8mb4_bin NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `identifier` varchar(40) NOT NULL,
+  `accounts` longtext DEFAULT NULL,
+  `group` varchar(50) DEFAULT 'user',
+  `inventory` longtext DEFAULT NULL,
+  `job` varchar(20) DEFAULT 'unemployed',
+  `job_grade` int(11) DEFAULT 0,
+  `job2` varchar(255) DEFAULT 'unemployed2',
+  `job2_grade` int(11) DEFAULT 0,
+  `org` varchar(255) DEFAULT 'unemployed2',
+  `org_gradeorg` int(11) DEFAULT 0,
+  `loadout` longtext DEFAULT NULL,
+  `position` varchar(255) DEFAULT '{"x":-269.4,"y":-955.3,"z":31.2,"heading":205.8}'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `org`
+--
+ALTER TABLE `org`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `organisation_moneywash`
+--
+ALTER TABLE `organisation_moneywash`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`identifier`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `organisation_moneywash`
+--
+ALTER TABLE `organisation_moneywash`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ es_extended/server/common.lua
@@ -0,0 +1,110 @@
+ESX = {}
+ESX.Players = {}
+ESX.UsableItemsCallbacks = {}
+ESX.Items = {}
+ESX.ServerCallbacks = {}
+ESX.TimeoutCount = -1
+ESX.CancelledTimeouts = {}
+ESX.Pickups = {}
+ESX.PickupId = 0
+ESX.Jobs = {}
+ESX.Org = {}
+ESX.RegisteredCommands = {}
+
+AddEventHandler('esx:getSharedObject', function(cb)
+	cb(ESX)
+end)
+
+function getSharedObject()
+	return ESX
+end
+
+MySQL.ready(function()
+	MySQL.Async.fetchAll('SELECT * FROM items', {}, function(result)
+		for k,v in ipairs(result) do
+			ESX.Items[v.name] = {
+				label = v.label,
+				weight = v.weight,
+				rare = v.rare,
+				canRemove = v.can_remove
+			}
+		end
+	end)
+
+	MySQL.Async.fetchAll('SELECT * FROM jobs', {}, function(jobs)
+		for k,v in ipairs(jobs) do
+			ESX.Jobs[v.name] = v
+			ESX.Jobs[v.name].grades = {}
+		end
+
+		MySQL.Async.fetchAll('SELECT * FROM job_grades', {}, function(jobGrades)
+			for k,v in ipairs(jobGrades) do
+				if ESX.Jobs[v.job_name] then
+					ESX.Jobs[v.job_name].grades[tostring(v.grade)] = v
+				else
+					print(('[es_extended] [^3WARNING^7] Ignoring job grades for "%s" due to missing job'):format(v.job_name))
+				end
+			end
+
+			for k2,v2 in pairs(ESX.Jobs) do
+				if ESX.Table.SizeOf(v2.grades) == 0 then
+					ESX.Jobs[v2.name] = nil
+					print(('[es_extended] [^3WARNING^7] Ignoring job "%s" due to no job grades found'):format(v2.name))
+				end
+			end
+		end)
+
+
+		MySQL.Async.fetchAll('SELECT * FROM job_grades', {}, function(job2Grades)
+			for k,v in ipairs(job2Grades) do
+				if ESX.Jobs[v.job_name] then
+					ESX.Jobs[v.job_name].grades[tostring(v.grade2)] = v
+				else
+					print(('[es_extended] [^3WARNING^7] Ignoring job2 grades for "%s" due to missing job'):format(v.job_name))
+				end
+			end
+		end)
+	end)
+	
+	local result = MySQL.Sync.fetchAll('SELECT * FROM org', {})
+
+	for i=1, #result do
+		ESX.Org[result[i].name] = result[i]
+		ESX.Org[result[i].name].gradeorg = {}
+	end
+
+	local result2 = MySQL.Sync.fetchAll('SELECT * FROM org_gradeorg', {})
+
+	for i=1, #result2 do
+		if ESX.Org[result2[i].org_name] then
+			ESX.Org[result2[i].org_name].gradeorg[tostring(result2[i].gradeorg)] = result2[i]
+		else
+			print(('es_extended: invalid org "%s" from table org_gradeorg ignored!'):format(result2[i].org_name))
+		end
+	end
+
+	for k,v in pairs(ESX.Org) do
+		if next(v.gradeorg) == nil then
+			ESX.Org[v.name] = nil
+			print(('es_extended: ignoring org "%s" due to missing org gradeorg!'):format(v.name))
+		end
+	end
+
+	print('[es_extended] [^2INFO^7] ESX developed by ESX-Org has been initialized')
+end)
+
+RegisterServerEvent('esx:clientLog')
+AddEventHandler('esx:clientLog', function(msg)
+	if Config.EnableDebug then
+		print(('[es_extended] [^2TRACE^7] %s^7'):format(msg))
+	end
+end)
+
+RegisterServerEvent('esx:triggerServerCallback')
+AddEventHandler('esx:triggerServerCallback', function(name, requestId, ...)
+	local playerId = source
+
+	ESX.TriggerServerCallback(name, requestId, playerId, function(...)
+		TriggerClientEvent('esx:serverCallback', playerId, requestId, ...)
+	end, ...)
+end)
